be8287f7e0f92553aab185ac256aea3db7718010
Benjamin Monchatre <benjamin.monchatre@gmail.com>
2019-07-23 10:50:10 +0200

    creation structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { ItemComponent } from './post-list/item/item.component';
+import { NewPostComponent } from './post-list/new-post/new-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    ItemComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
